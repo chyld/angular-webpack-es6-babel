c67d71c785d7b494d1962320edb8adf347024bc7
Chyld Medford <chyld.medford@gmail.com>
2015-10-26 14:46:02 -0700

    highcharts

--- client/controllers/sales/list.js
+++ client/controllers/sales/list.js
@@ -1,4 +1,22 @@
 export default function($scope, Sale, $state){
   $scope.name = 'List';
-  Sale.all().then(r => $scope.sales = r.data);
+  Sale.all().then(r => {
+    $scope.sales = r.data;
+    let categories = r.data.map(d => d.id);
+    let amounts = r.data.map(d => d.amount * 1);
+    draw(categories, amounts);
+  });
+}
+
+function draw(categories, data){
+  debugger;
+  $('#container').highcharts({
+    title: {
+        text: 'Sale amounts over time'
+    },
+    xAxis: {
+        categories: categories
+    },
+    series: [{data: data}]
+  });
 }
